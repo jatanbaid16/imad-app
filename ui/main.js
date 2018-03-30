d4cc3bf3e7642b29df72cfb647e3c1d678827dac
jatanbaid16 <36809578+jatanbaid16@users.noreply.github.com>
2018-03-30 19:26:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+//change the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.marginleft='100px';
+    
+};
