3a5b5400b3b5b8dc6c74c691b50ec072c7084717
jatanbaid16 <36809578+jatanbaid16@users.noreply.github.com>
2018-03-30 15:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,84 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article One | Jatan Baid',
+    heading:'Article One',
+    date:'30 march,2018',
+    content:`
+    <p>
+                This is the content for my first article.
+                This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
+            </p>
+        </div>
+        <div>
+            
+            <p>
+                This is the content for my first article.
+                This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article. 
+            </p>
+            `
+    
+
+};
+function createTempleate(data){
+var titile=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+    var htmlTemplate=`
+    
+    <!DOCTYPE html>
+    <head>
+        <title>
+            ${title}
+            
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width-device-width.intial-scale=1" >
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <a href="/">HOME</a>
+            </div>
+            <hr>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+                
+                ${content}
+            </div>
+        </div>
+    </body>
+    
+    
+    </html>
+    
+    
+    `;
+    return htmlTemplate;
+}
+
+
+
+
+
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(craeteTemplate(articleOne)); 
 });
 
 app.get('/article-two', function (req, res){
