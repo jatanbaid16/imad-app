a8bdd5e936159c7692ed678324726076974dea94
jatanbaid16 <36809578+jatanbaid16@users.noreply.github.com>
2018-03-31 01:48:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ marginLeft=marginLeft+10;
 img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
-    var interval=setinterval(moveRight,100);
+    var interval=setInterval(moveRight,100);
     
 };
