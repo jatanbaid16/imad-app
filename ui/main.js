c23ac5f02b4287bcb1d62783adcbb9efc2ddd5c9
jatanbaid16 <36809578+jatanbaid16@users.noreply.github.com>
2018-04-13 16:27:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -54,7 +54,7 @@ submit.onclick=function(){
     //Make the request
     var nameInput=document.getElementById('name');
     var name=nameInput.value;
-    request.open('GET','http://jatanbaid16.imad.hasura-app.io/submit-name?name='+name,true);
+    request.open('GET','http://jatanbaid16.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
     
     
